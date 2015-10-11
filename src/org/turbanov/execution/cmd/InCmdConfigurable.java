497e9be5d95b4899427402dfdd61445d57d6dd3c
turbanoff <turbanoff@gmail.com>
2015-10-11 13:58:02 +0300

    implement plugin settings

---
+++ src/org/turbanov/execution/cmd/InCmdConfigurable.java
@@ -0,0 +1,93 @@
+package org.turbanov.execution.cmd;
+
+import com.intellij.openapi.components.ServiceManager;
+import com.intellij.openapi.options.Configurable;
+import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.LabeledComponent;
+import com.intellij.util.ui.JBUI;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author Andrey Turbanov
+ */
+public class InCmdConfigurable implements Configurable  {
+
+    private final Project myProject;
+    private OptionsPatchConfiguration myState;
+    private JTextArea toAddVmOptions;
+    private JTextArea toRemoveVmOptions;
+    private JTextArea toAddProgramOptions;
+    private JTextArea toRemoveProgramOptions;
+
+    public InCmdConfigurable(Project project) {
+        myProject = project;
+    }
+
+    @Nls
+    @Override
+    public String getDisplayName() {
+        return "RunInCmd plugin";
+    }
+
+    @Nullable
+    @Override
+    public String getHelpTopic() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public JComponent createComponent() {
+        myState = ServiceManager.getService(myProject, OptionsPatchConfiguration.class);
+
+        GridLayout layout = new GridLayout(4, 1, JBUI.scale(3), JBUI.scale(3));
+        JPanel result = new JPanel(layout);
+
+        toAddVmOptions = new JTextArea(myState.toAddVmOptions);
+        toRemoveVmOptions = new JTextArea(myState.toRemoveVmOptions);
+        toAddProgramOptions = new JTextArea(myState.toAddProgramOptions);
+        toRemoveProgramOptions = new JTextArea(myState.toRemoveProgramOptions);
+
+        result.add(LabeledComponent.create(toAddVmOptions, "To add VM options"));
+        result.add(LabeledComponent.create(toRemoveVmOptions, "To remove VM options"));
+        result.add(LabeledComponent.create(toAddProgramOptions, "To add program options"));
+        result.add(LabeledComponent.create(toRemoveProgramOptions, "To remove program options"));
+
+        return result;
+    }
+
+    @Override
+    public boolean isModified() {
+        return !toAddVmOptions.getText().equals(myState.toAddVmOptions)
+            || !toRemoveVmOptions.getText().equals(myState.toRemoveVmOptions)
+            || !toAddProgramOptions.getText().equals(myState.toAddProgramOptions)
+            || !toRemoveProgramOptions.getText().equals(myState.toRemoveProgramOptions);
+    }
+
+    @Override
+    public void apply() throws ConfigurationException {
+        OptionsPatchConfiguration state = new OptionsPatchConfiguration();
+        state.toAddVmOptions = toAddVmOptions.getText();
+        state.toRemoveVmOptions = toRemoveVmOptions.getText();
+        state.toAddProgramOptions = toAddProgramOptions.getText();
+        state.toRemoveProgramOptions = toRemoveProgramOptions.getText();
+        ServiceManager.getService(myProject, OptionsPatchConfiguration.class).loadState(state);
+    }
+
+    @Override
+    public void reset() {
+        toAddVmOptions.setText(myState.toAddVmOptions);
+        toRemoveVmOptions.setText(myState.toRemoveVmOptions);
+        toAddProgramOptions.setText(myState.toAddProgramOptions);
+        toRemoveProgramOptions.setText(myState.toRemoveProgramOptions);
+    }
+
+    @Override
+    public void disposeUIResources() {
+    }
+}
---
+++ src/org/turbanov/execution/cmd/OptionsPatchConfiguration.java
@@ -0,0 +1,33 @@
+package org.turbanov.execution.cmd;
+
+import com.intellij.openapi.components.PersistentStateComponent;
+import com.intellij.openapi.components.State;
+import com.intellij.openapi.components.Storage;
+import com.intellij.openapi.components.StoragePathMacros;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author Andrey Turbanov
+ */
+@State(name = "RunInCmdPluginSettings", storages = {@Storage(file = StoragePathMacros.PROJECT_CONFIG_DIR + "/RunInCmd.xml")})
+public class OptionsPatchConfiguration implements PersistentStateComponent<OptionsPatchConfiguration> {
+
+    public String toAddVmOptions = "";
+    public String toRemoveVmOptions = "";
+    public String toAddProgramOptions = "";
+    public String toRemoveProgramOptions = "";
+
+    @NotNull
+    @Override
+    public OptionsPatchConfiguration getState() {
+        return this;
+    }
+
+    @Override
+    public void loadState(OptionsPatchConfiguration state) {
+        toAddVmOptions = state.toAddVmOptions;
+        toRemoveVmOptions = state.toRemoveVmOptions;
+        toAddProgramOptions = state.toAddProgramOptions;
+        toRemoveProgramOptions = state.toRemoveProgramOptions;
+    }
+}
